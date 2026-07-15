ccdd3a963cf61121c803d582e006861e82eb2703
akbarwidya <akbarwidya.dev@gmail.com>
2026-07-16 03:47:26 +0700

    Update ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-flf5r$u)$t@s_sm6b%$oq0rvjx4m62gm%g(g@1#)#(b!kcj-ub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
